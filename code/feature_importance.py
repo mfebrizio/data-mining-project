db6b11b170771755a48560df8dafd53083321c71
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2022-06-21 19:19:05 -0400

    Update feature_importance.py

--- code/feature_importance.py
+++ code/feature_importance.py
@@ -38,8 +38,9 @@ a = 622
 
 # column dtypes
 label = 'type'
-numeric_features = ['page_length', 'agencies_count_uq', 'abstract_length', 'page_views_count', 'RIN_count', 'CFR_ref_count']
-categorical_features = ['sig', 'effective_date_exists', 'comments_close_exists', 'docket_exists']
+numeric_features = ['page_length', 'agencies_count_uq', 'abstract_length', 'page_views_count',
+                    'RIN_count', 'CFR_ref_count']
+categorical_features = ['sig', 'effective_date_exists', 'comments_close_exists', 'docket_exists', 'eop']
 
 # distinguish X and y objects
 X = df[numeric_features+categorical_features]
@@ -52,7 +53,7 @@ print("Encoded classes: ", list(zip(range(0,4),le.classes_)), sep="\n")
 
 # transformers by dtype
 numeric_transformer = StandardScaler()
-categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+categorical_transformer = OneHotEncoder(drop="if_binary")
 
 # preprocessor
 preprocessor = ColumnTransformer(
@@ -83,6 +84,15 @@ feature_selection = Pipeline(
 feature_selection.fit(X, y_enc)
 
 # print results
+print(f"Estimator: {selector.estimator_}",
+          f"No. features in: {feature_selection.n_features_in_}",
+          f"Feature names in: {feature_selection.feature_names_in_}",
+          f"Threshold value ({selector_threshold}): {selector.threshold_}",
+          f"Mask of features selected: {selector.get_support(indices=True)}",
+          f"Mask of selected feature names: {feature_selection.get_feature_names_out()}",
+          sep="\n")
+
+# save output
 save_dir = p.parent.joinpath('data', 'analysis')
 savePath = save_dir / r"feature_selection.txt"
 with open(savePath, 'w') as textfile:
